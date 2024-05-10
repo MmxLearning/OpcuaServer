9fe5416687d4f98101128a54bc3f20271e097db5
Mmx233 <green.dusk8264@fastmail.com>
2024-05-10 18:36:26 +0800

    init: mysql connect

---
+++ internal/db/dao/init.go
@@ -0,0 +1,27 @@
+package dao
+
+import (
+	"github.com/MmxLearning/OpcuaServer/internal/global"
+	"github.com/MmxLearning/OpcuaServer/pkg/drivers/mysql"
+	log "github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func init() {
+	conf := &gorm.Config{
+		PrepareStmt:            true,
+		SkipDefaultTransaction: true,
+	}
+
+	var err error
+	DB, err = mysql.New(&global.Config.Mysql, conf)
+	if err != nil {
+		log.Fatalln("连接 Mysql 失败:", err)
+	}
+
+	if err = DB.AutoMigrate(); err != nil {
+		log.Fatalln("AutoMigration failed:", err)
+	}
+}
