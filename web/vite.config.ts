bf817b144244c374e5f5ebcd0f349e9b444b6747
Mmx233 <green.dusk8264@fastmail.com>
2024-05-11 23:35:12 +0800

    init: prepare web codes

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
+import path from "path";
+
+const __dirname = path.resolve();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            "babel-plugin-direct-import",
+            {
+              modules: ["@mui/material", "@mui/icons-material", "@mui/lab"],
+            },
+          ],
+        ],
+      },
+    }),
+    legacy(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@store": path.resolve(__dirname, "src/store"),
+    },
+  },
+});
